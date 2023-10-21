a8e4174f03cd7163522da8fdb83df34d405426df
kun99 <kuns.km@gmail.com>
2023-10-22 01:43:24 +0700

    updates

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="id">
     <h1>Login</h1>
-    <input v-model="username" placeholder="Username">
-    <input v-model="password" placeholder="Password" type="password">
+    <input v-model="username" placeholder="Username" />
+    <input v-model="password" placeholder="Password" type="password" />
   </div>
   <div class="submit">
-    <button @click="cancel" style="margin-right: 10px;">Cancel</button>
+    <button @click="cancel" style="margin-right: 10px">Cancel</button>
     <button @click="login">Login</button>
   </div>
 </template>
-  
+
 <style>
 .id {
   font-size: 20px;
@@ -28,31 +28,35 @@
 </style>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
+import { useAuthStore } from "../stores/store.js";
 export default {
   data() {
     return {
-      username: '',
-      password: ''
+      username: "",
+      password: "",
     };
   },
   methods: {
     login() {
-      axios.post('http://localhost:5000/login', {
-        username: this.username,
-        password: this.password
-      })
-      .then(response => {
-        console.log(response.data);
-      })
-      .catch(error => {
-        console.error(error);
-      });
+      axios
+        .post("http://localhost:5000/login", {
+          username: this.username,
+          password: this.password,
+        })
+        .then((response) => {
+          const auth = useAuthStore();
+          auth.setToken(response.data.token);
+          //console.log(response.data.token);
+          window.location.href = "http://localhost:4000";
+        })
+        .catch((error) => {
+          console.error(error);
+        });
     },
     cancel() {
-      this.$router.push('/');
-    }
-  }
+      this.$router.push("/");
+    },
+  },
 };
 </script>
-  
